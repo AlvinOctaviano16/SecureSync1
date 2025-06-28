a8b217d987d29d3cf00278a883d3a4a0331378ad
Muh Rizqullah Akbar <inirizqullahakbar@gmail.com>
2025-06-28 08:55:56 +0800

    perbaiki __init__.py

--- Keamanan_Apk/app/__init__.py
+++ Keamanan_Apk/app/__init__.py
@@ -1,39 +1,36 @@
-# app/__init__.py
 import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 from cryptography.fernet import Fernet
 from .routes import main_bp
-from .models import db, User # Import User untuk user_loader
+from .models import db, User 
 
-# db = SQLAlchemy() # Pindahkan ke models.py
 login_manager = LoginManager()
 
 def create_app():
     # Inisialisasi Flask aplikasi
-    app = Flask(__name__, instance_relative_config=True) # Tambahkan instance_relative_config
+    # Beri tahu Flask di mana folder static berada
+    app = Flask(__name__, instance_relative_config=True, static_folder='../static') # <-- Perbaikan di sini!
     
     # Konfigurasi aplikasi
-    app.config['SECRET_KEY'] = os.urandom(24) # Gunakan kunci acak yang lebih aman
+    app.config['SECRET_KEY'] = os.urandom(24)
     app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
     # Konfigurasi kunci enkripsi Fernet
-    # Pastikan kunci ini tetap sama untuk dekripsi data!
     app.config['FERNET_KEY'] = b't7tqK5Ba9xS6eQHgN6GLpF94cYlgdAKxY-tAVmO14y0='
     app.config['CIPHER_SUITE'] = Fernet(app.config['FERNET_KEY'])
 
     # Inisialisasi ekstensi dengan aplikasi
     db.init_app(app)
     login_manager.init_app(app)
-    login_manager.login_view = 'main.login_register' # Tentukan rute login
+    login_manager.login_view = 'main.login_register'
     
     # User loader untuk Flask-Login
     @login_manager.user_loader
     def load_user(user_id):
         with app.app_context():
-            # Pastikan User diimpor dari .models
             return User.query.get(int(user_id))
 
     # Daftarkan Blueprint di sini
@@ -48,7 +45,6 @@ def create_tables_and_admin(app):
         # Membuat user admin default jika belum ada
         if not User.query.filter_by(username='admin').first():
             admin_user = User(username='admin', role='admin')
-            # set_password dari model akan menggunakan bcrypt
             admin_user.set_password('adminpassword') 
             db.session.add(admin_user)
             db.session.commit()
